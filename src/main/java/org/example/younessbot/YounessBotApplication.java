f2c7d458b01de08ccd3c54180d45b5571edbb4ac
Youness <younesskossara@gmail.com>
2025-12-18 11:11:16 +0100

    Initial commit - projet Spring AI OpenAI

---
+++ src/main/java/org/example/younessbot/YounessBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.younessbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounessBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YounessBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/younessbot/YounessBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.younessbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YounessBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
